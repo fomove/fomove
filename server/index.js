67cdb7083c4ff522bacfcb66703cf9feb8323ab1
Andrey Barchuk <andreyb@123software.ru>
2019-04-25 11:13:55 +0300

    test for heroku

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const router = express.Router();
-const multer  = require('multer');
-const cloudinary = require('cloudinary');
-const cloudinaryStorage = require('multer-storage-cloudinary');
+// const multer  = require('multer');
+// const cloudinary = require('cloudinary');
+// const cloudinaryStorage = require('multer-storage-cloudinary');
 const fs = require('fs');
 const PORT = process.env.PORT || 5000;
 
-cloudinary.config({ 
-  cloud_name: 'deliciousshop64', 
-  api_key: '668657522175832', 
-  api_secret: '8vn0lRyG9IHPsNqBz6_6dWzHQdY' 
-});
+// cloudinary.config({
+//   cloud_name: 'deliciousshop64',
+//   api_key: '668657522175832',
+//   api_secret: '8vn0lRyG9IHPsNqBz6_6dWzHQdY'
+// });
 // Multi-process to utilize all CPU cores.
 // if (cluster.isMaster) {
 //   console.error(`Node cluster master ${process.pid} is running`);
@@ -31,21 +31,21 @@ cloudinary.config({
 // } else {
   const app = express();
 
-  let storage = cloudinaryStorage({
-    cloudinary: cloudinary,
-    folder: '', // cloudinary folder where you want to store images, empty is root
-    allowedFormats: ['jpg', 'png'],
-  });
+  // let storage = cloudinaryStorage({
+  //   cloudinary: cloudinary,
+  //   folder: '', // cloudinary folder where you want to store images, empty is root
+  //   allowedFormats: ['jpg', 'png'],
+  // });
   
   /* Initialize multer middleware with the multer-storage-cloudinary based
      storage engine */
-  var parser = multer({ storage: storage });
+  // var parser = multer({ storage: storage });
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
   // Answer API requests.
 
   // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 
 
@@ -58,11 +58,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
       "test3": "cheburek"
     });
   });
-  app.post('/image/upload', parser.single('selectedFile'), function (req, res) {       
-    console.log(req.file);
-    res.json(req.file); 
-  });
-
+  // app.post('/image/upload', parser.single('selectedFile'), function (req, res) {
+  //   console.log(req.file);
+  //   res.json(req.file);
+  // });sd
 
   app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
